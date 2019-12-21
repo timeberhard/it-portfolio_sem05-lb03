176176156fe40886a2288fc585877990fbabf9f1
timeberhard <timeberhard@bluewin.ch>
2019-12-21 18:23:51 +0100

    sass complete, Leiterverzeichnis, formular (fail)

---
+++ js/form.js
@@ -0,0 +1,37 @@
+// JavaScript Document
+
+function getData() {
+	var name = document.getElementById("name").value;
+	var email = document.getElementById("email");
+	var message = document.getElementById("message");
+	var request = {
+		name,
+		surname,
+		email,
+		date,
+		message
+	};
+	this.Request = request;
+	console.log(this.Request);
+	this.sendEmail();
+}
+
+function sendEmail() {
+  var mail =
+    "mailto:" +
+    Request.email +
+    "?subject=" +
+    Request.name +
+    " " +
+    Request.surname +
+    "&body=" +
+    "Vielen Dank for Deine Fotoshooting Anfrage" +
+    " " +
+    Request.surname +
+    "%0D%0A" +
+    "%0D%0A" +
+    Request.message;
+  var a = document.createElement("a");
+  a.href = mail;
+  a.click();
+}
